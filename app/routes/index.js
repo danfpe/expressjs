cfc179ca161e08567e84211d5f4af1d28fb5e0f4
DanfaPeng <danfa.peng@outlook.com>
2017-03-10 09:36:13 +0800

    the first edition

---
+++ app/routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  var data = req.app.get('appData');
+  var pagePhotos = [];
+  var pageSpeakers = data.speakers;
+  var otherSpeakers=data.others;
+
+  data.speakers.forEach(function(item) {
+    pagePhotos = pagePhotos.concat(item.artwork);
+  });
+
+  res.render('index', {
+    pageTitle: 'Home',
+    artwork: pagePhotos,
+    speakers: pageSpeakers,
+    others: otherSpeakers,
+    pageID: 'home'
+  });
+
+});
+
+module.exports = router;
